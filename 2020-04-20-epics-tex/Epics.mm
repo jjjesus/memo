85b1fa65c166ce5ba7dbd8ddbbd766b186c3a440
John Jesus <john.j.jesus@gmail.com>
2020-04-19 06:18:49 -0500

    Start Epics mindmap.

--- 2020-04-20-epics-tex/Epics.mm
+++ 2020-04-20-epics-tex/Epics.mm
@@ -1,4 +1,8 @@
 <map version="1.1.0">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1587293966653" ID="ID_129984678" MODIFIED="1587293974590" TEXT="Epics"/>
+<node CREATED="1587293966653" ID="ID_129984678" MODIFIED="1587293974590" TEXT="Epics">
+<node CREATED="1587295073168" ID="ID_582235914" MODIFIED="1587295075961" POSITION="right" TEXT="BLUF">
+<node CREATED="1587295067356" ID="ID_1666537271" MODIFIED="1587295069604" TEXT="Communicate K3 software development plans and progress using Releases and Epics instead of using only Stories."/>
+</node>
+</node>
 </map>
