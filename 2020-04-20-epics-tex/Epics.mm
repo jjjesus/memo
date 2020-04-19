8a36eb817063d7e232ccb67a918b7817083c29f5
John Jesus <john.j.jesus@gmail.com>
2020-04-19 06:01:39 -0500

    Epics mindmap.

---
+++ 2020-04-20-epics-tex/Epics.mm
@@ -0,0 +1,4 @@
+<map version="1.1.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1587293966653" ID="ID_129984678" MODIFIED="1587293974590" TEXT="Epics"/>
+</map>
