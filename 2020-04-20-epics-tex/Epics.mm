2290b225e3262e65a9341ef95f7e3defff42455a
John Jesus <john.j.jesus@gmail.com>
2020-04-19 07:29:15 -0500

    Finished mindmap outline.

--- 2020-04-20-epics-tex/Epics.mm
+++ 2020-04-20-epics-tex/Epics.mm
@@ -4,5 +4,38 @@
 <node CREATED="1587295073168" ID="ID_582235914" MODIFIED="1587295075961" POSITION="right" TEXT="BLUF">
 <node CREATED="1587295067356" ID="ID_1666537271" MODIFIED="1587295069604" TEXT="Communicate K3 software development plans and progress using Releases and Epics instead of using only Stories."/>
 </node>
+<node CREATED="1587298515996" ID="ID_1668386463" MODIFIED="1587298524172" POSITION="right" TEXT="My experience in IIS">
+<node CREATED="1587298524655" ID="ID_1922178542" MODIFIED="1587298533654" TEXT="1 Release every 18 months"/>
+<node CREATED="1587298534468" ID="ID_20629413" MODIFIED="1587298544503" TEXT="Release contained 8-10 Features"/>
+<node CREATED="1587298545158" ID="ID_691988095" MODIFIED="1587298581128" TEXT="Feature is specified as an Epic"/>
+<node CREATED="1587298583359" ID="ID_210603119" MODIFIED="1587298585595" TEXT="Example">
+<node CREATED="1587298585603" ID="ID_1952905300" MODIFIED="1587298587948" TEXT="Release 9.5">
+<node CREATED="1587298594922" ID="ID_332434166" MODIFIED="1587298607361" TEXT="Cloud Cover Pct on videos"/>
+<node CREATED="1587298607785" ID="ID_1801840297" MODIFIED="1587298612356" TEXT="File formats"/>
+<node CREATED="1587298613349" ID="ID_161015250" MODIFIED="1587298615782" TEXT="Data providers"/>
+<node CREATED="1587298616809" ID="ID_233805332" MODIFIED="1587298619307" TEXT="OS upgrade"/>
+<node CREATED="1587298620087" ID="ID_595631313" MODIFIED="1587298626065" TEXT="Quarterly traffic report"/>
+</node>
+</node>
+<node CREATED="1587298716869" ID="ID_605473819" MODIFIED="1587298734273" TEXT="Last 3-week interval before release is an Integration period">
+<node CREATED="1587298737075" ID="ID_614837836" MODIFIED="1587298740299" TEXT="No stories"/>
+<node CREATED="1587298741116" ID="ID_1162816476" MODIFIED="1587298743446" TEXT="Merging"/>
+<node CREATED="1587298744234" ID="ID_1065520513" MODIFIED="1587298747660" TEXT="Integration testing"/>
+<node CREATED="1587298751979" ID="ID_330227896" MODIFIED="1587298754019" TEXT="Bug fixes"/>
+<node CREATED="1587298758077" ID="ID_1466896761" MODIFIED="1587298760032" TEXT="Documents"/>
+<node CREATED="1587298957384" ID="ID_1634483392" MODIFIED="1587298998866" TEXT="Reviews and release procedures"/>
+<node CREATED="1587298760692" ID="ID_1995398045" MODIFIED="1587298762873" TEXT="Packaging"/>
+</node>
+</node>
+<node CREATED="1587298647890" ID="ID_286009826" MODIFIED="1587298655720" POSITION="right" TEXT="Releases are calendar-based"/>
+<node CREATED="1587298660758" ID="ID_152186769" MODIFIED="1587298669154" POSITION="right" TEXT="Epics are features delivered in a Release">
+<node CREATED="1587298908301" ID="ID_1437329146" MODIFIED="1587298913268" TEXT="Atlassian documents">
+<node CREATED="1587298867189" ID="ID_706219527" MODIFIED="1587298869008" TEXT="https://www.atlassian.com/agile/project-management/epics"/>
+<node CREATED="1587298870241" ID="ID_127652486" MODIFIED="1587298896659" TEXT="Epics are almost always delivered over a set of sprints. As a team learns more about an epic through development and customer feedback, user stories will be added and removed as necessary. That&#x2019;s the key with agile epics: Scope is flexible, based on customer feedback and team cadence."/>
+<node CREATED="1587299145812" ID="ID_1633253745" MODIFIED="1587299147163" TEXT="Breaking initiatives into epics helps keep the team&#x2019;s daily work &#x2014; expressed in smaller stories &#x2014; connected to overall business goals."/>
+</node>
+</node>
+<node CREATED="1587298678401" ID="ID_529554803" MODIFIED="1587298688625" POSITION="right" TEXT="Releases and epics are communicated outside the group/sprint team"/>
+<node CREATED="1587298690165" ID="ID_1708288836" MODIFIED="1587298704822" POSITION="right" TEXT="Stories are not communicated to the customer or outside the group/sprint team"/>
 </node>
 </map>
